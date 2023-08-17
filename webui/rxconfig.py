b69db2d08543b5c4d7854cd56fcf4288576ad80e
Nev Delap <nevdelap@gmail.com>
2023-08-17 10:37:37 +0000

    Domain.

--- webui/rxconfig.py
+++ webui/rxconfig.py
@@ -7,7 +7,7 @@ class WebuiConfig(rx.Config):
 
 config = WebuiConfig(
     app_name="webui",
-    api_url="http://170.64.132.193:8000",
+    api_url="http://nevdelap.com:8000",
     db_url="sqlite:///reflex.db",
     frontend_packages=[
         "react-loading-icons",
