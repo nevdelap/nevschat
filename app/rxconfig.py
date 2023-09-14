3e232721c5ed4b4ff3f13083d17b09d5d3265a27
Nev Delap <nevd@benon.com>
2023-09-14 19:07:04 +1000

    Fix config.

--- app/rxconfig.py
+++ app/rxconfig.py
@@ -1,17 +1,26 @@
+# mypy: disable-error-code="attr-defined,name-defined"
+
 import socket
 
 import reflex as rx
 
-config = rx.Config(  # type: ignore
+PROD_HOSTNAMES = [
+    "buildkitsandbox",  # Buildtime docker container.
+    "prod",  # Prod runtime docker container.
+]
+
+config = rx.Config(
     app_name="nevschat",
     api_url=(
         "https://nevdelap.com:8000"
-        if socket.gethostname() == "droplet"
+        if socket.gethostname() in PROD_HOSTNAMES
         else "http://localhost:8000"
     ),
     db_url="sqlite:///reflex.db",
     frontend_packages=[
         "react-loading-icons",
     ],
-    frontend_path="/chat",
+    frontend_path=(
+        "/chat" if socket.gethostname() in PROD_HOSTNAMES else ""
+    ),
 )
