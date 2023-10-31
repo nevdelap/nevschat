18000a2e7804346a649a35b44f3b10c2a4f56c8c
Nev Delap <nevd@benon.com>
2023-10-31 17:08:18 +1000

    Add a version number.

--- app/nevschat/nevschat.py
+++ app/nevschat/nevschat.py
@@ -9,7 +9,7 @@ from nevschat.state import State
 def index() -> rx.Component:
     return rx.vstack(
         rx.heading(
-            "Nev's Awesome ChatGPT",
+            "Nev's Awesome ChatGPT v2",
             text_align="left",
             size="lg",
             width="100%",
