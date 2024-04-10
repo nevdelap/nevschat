f04962cfa8c2a1dc584a6ca6e36238d3a9658274
Nev Delap <nevdelap@gmail.com>
2024-04-10 17:39:37 +1000

    Comment out on_submit, needs harder looking at. Is getting handleSubmit_hexhexhex is not defined.

--- app/nevschat/components/chat.py
+++ app/nevschat/components/chat.py
@@ -12,43 +12,43 @@ def prompt_response_box(prompt_response: PromptResponse, index: int) -> rx.Compo
     return rx.box(
         rx.cond(
             prompt_response.is_editing,
-            rx.form(
-                rx.hstack(
-                    # rx.debounce_input(
-                    rx.text_area(
-                        enter_key_submit=True,
-                        on_change=State.update_edited_prompt,
-                        width="100%",
-                        # ),
-                        # debounce_timeout=250,
-                        # value=State.edited_prompt,
-                    ),
-                    rx.button(
-                        rx.icon(tag="eraser"),
-                        color_scheme="red",
-                        disabled=State.cannot_clear_or_send_edited_prompt,
-                        on_click=lambda: State.clear_edited_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
-                        margin_top="0.75em",
-                    ),
-                    rx.button(
-                        rx.icon(tag="send-horizontal"),
-                        color_scheme="grass",
-                        disabled=State.cannot_clear_or_send_edited_prompt,
-                        on_click=lambda: State.send_edited_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
-                        margin_top="0.75em",
-                        type="submit",
-                    ),
-                    rx.button(
-                        rx.icon(tag="x"),
-                        color_scheme="tomato",
-                        on_click=lambda: State.cancel_edit_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
-                        margin_top="0.75em",
-                    ),
+            # rx.form(
+            rx.hstack(
+                # rx.debounce_input(
+                rx.text_area(
+                    # enter_key_submit=True,
+                    on_change=State.update_edited_prompt,
                     width="100%",
+                    # ),
+                    # debounce_timeout=250,
+                    # value=State.edited_prompt,
                 ),
-                on_submit=lambda _form_data_unused: State.send_edited_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
+                rx.button(
+                    rx.icon(tag="eraser"),
+                    color_scheme="red",
+                    disabled=State.cannot_clear_or_send_edited_prompt,
+                    on_click=lambda: State.clear_edited_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
+                    margin_top="0.75em",
+                ),
+                rx.button(
+                    rx.icon(tag="send-horizontal"),
+                    color_scheme="grass",
+                    disabled=State.cannot_clear_or_send_edited_prompt,
+                    on_click=lambda: State.send_edited_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
+                    margin_top="0.75em",
+                    # type="submit",
+                ),
+                rx.button(
+                    rx.icon(tag="x"),
+                    color_scheme="tomato",
+                    on_click=lambda: State.cancel_edit_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
+                    margin_top="0.75em",
+                ),
                 width="100%",
             ),
+            # on_submit=lambda _form_data_unused: State.send_edited_prompt(index),  # type: ignore  # pylint: disable=no-value-for-parameter
+            #     width="100%",
+            # ),
             rx.hstack(
                 rx.box(
                     rx.markdown(prompt_response.prompt),
@@ -166,39 +166,39 @@ def chat() -> rx.Component:
             ),
             width="100%",
         ),
-        rx.form(
-            rx.hstack(
-                # rx.debounce_input(
-                rx.text_area(
-                    disabled=State.cannot_enter_new_prompt_or_edit,
-                    enter_key_submit=True,
-                    on_change=State.set_new_prompt,
-                    placeholder="Ask something.",
-                    width="100%",
-                    # ),
-                    # debounce_timeout=250,
-                    # value=State.new_prompt,
-                ),
-                rx.button(
-                    rx.icon(tag="eraser"),
-                    color_scheme="red",
-                    disabled=State.cannot_send_new_prompt,
-                    on_click=lambda: State.clear_new_prompt,
-                    margin_top="0.75em",
-                ),
-                rx.button(
-                    rx.icon(tag="send-horizontal"),
-                    color_scheme="grass",
-                    disabled=State.cannot_send_new_prompt,
-                    is_loading=State.is_processing,
-                    on_click=State.send,
-                    margin_top="0.75em",
-                    type="submit",
-                ),
+        # rx.form(
+        rx.hstack(
+            # rx.debounce_input(
+            rx.text_area(
+                disabled=State.cannot_enter_new_prompt_or_edit,
+                # enter_key_submit=True,
+                on_change=State.set_new_prompt,
+                placeholder="Ask something.",
                 width="100%",
+                # ),
+                # debounce_timeout=250,
+                # value=State.new_prompt,
             ),
-            on_submit=lambda _form_data_unused: State.send(),  # type: ignore  # pylint: disable=no-value-for-parameter
+            rx.button(
+                rx.icon(tag="eraser"),
+                color_scheme="red",
+                disabled=State.cannot_send_new_prompt,
+                on_click=lambda: State.clear_new_prompt,
+                margin_top="0.75em",
+            ),
+            rx.button(
+                rx.icon(tag="send-horizontal"),
+                color_scheme="grass",
+                disabled=State.cannot_send_new_prompt,
+                is_loading=State.is_processing,
+                on_click=State.send,
+                margin_top="0.75em",
+                # type="submit",
+            ),
             width="100%",
         ),
+        # on_submit=lambda _form_data_unused: State.send(),  # type: ignore  # pylint: disable=no-value-for-parameter
+        #     width="100%",
+        # ),
         width="100%",
     )
--- app/nevschat/nevschat.py
+++ app/nevschat/nevschat.py
@@ -5,7 +5,7 @@ from nevschat.state import State
 
 import reflex as rx
 
-VERSION = 29
+VERSION = 30
 TITLE = "Nev's Awesome ChatGPT"
 
 
