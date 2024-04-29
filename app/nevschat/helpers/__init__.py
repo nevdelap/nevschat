08d8d7cc7d4b20b3e7b17f70f2b70cce66fd1aad
Nev Delap <nevdelap@gmail.com>
2024-04-29 16:33:42 +1000

    Factor out profile helpers.

--- app/nevschat/helpers/__init__.py
+++ app/nevschat/helpers/__init__.py
@@ -1,5 +1,13 @@
 from .cleanup import delete_old_wav_assets
+from .profile import get_random_is_male
+from .profile import get_random_profile
 from .tts import get_random_voice
 from .tts import text_to_wav
 
-__all__ = ["delete_old_wav_assets", "get_random_voice", "text_to_wav"]
+__all__ = [
+    "delete_old_wav_assets",
+    "get_random_is_male",
+    "get_random_profile",
+    "get_random_voice",
+    "text_to_wav",
+]
