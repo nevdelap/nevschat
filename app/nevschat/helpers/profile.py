b5be86b391b91a8bd777916c6e55c712d930a0d2
Nev Delap <nevdelap@gmail.com>
2024-05-01 20:08:11 +1000

    Add favourite foods and drinks.

--- app/nevschat/helpers/profile.py
+++ app/nevschat/helpers/profile.py
@@ -5,6 +5,7 @@ import reflex as rx
 from .japanese_text import age_to_kanji
 from .random_things import get_random_age
 from .random_things import get_random_city
+from .random_things import get_random_foods_and_drinks
 from .random_things import get_random_hobbies
 from .random_things import get_random_mood
 from .random_things import get_random_name
@@ -26,6 +27,7 @@ class Profile(rx.Base):  # type: ignore
     city = get_random_city()
     profession = get_random_profession(age)
     hobbies = get_random_hobbies(age)
+    foods_and_drinks = get_random_foods_and_drinks()
     mood = get_random_mood()
     pitch = get_random_pitch(age)
     speaking_rate = get_random_speaking_rate()
@@ -54,5 +56,6 @@ class Profile(rx.Base):  # type: ignore
         return (
             f"{pronoun}は{self.name}、{age_to_kanji(self.age)}歳で、"
             f"{self.city}に住んでいます。{self.profession}で、"
-            f"趣味は{self.hobbies}です。今私は{self.mood}"
+            f"趣味は{self.hobbies}です。{self.foods_and_drinks}が好きです。"
+            f"今私は{self.mood}"
         )
