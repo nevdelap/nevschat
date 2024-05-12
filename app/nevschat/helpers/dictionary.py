843b091b776316e3ebfbe32cadb1eaa8673ec320
Nev Delap <nevdelap@gmail.com>
2024-05-12 23:54:39 +1000

    Return dictionary matches with exact matches listed first.

--- app/nevschat/helpers/dictionary.py
+++ app/nevschat/helpers/dictionary.py
@@ -6,7 +6,15 @@ _jam: Final = Jamdict()
 
 
 def get_definition(text: str) -> str | None:
+    """
+    Return definitions for the given text, with exact matches first.
+    """
+    result = _jam.lookup(text)
+    entries = [entry.text(True) for entry in result.entries]
     result = _jam.lookup(f'{text}%')
-    if len(result.entries) > 0:
-        return ''.join(entry.text(True) + '\n' for entry in result.entries)
-    return 'Nothing found.'
+    entries += [
+        text for entry in result.entries if (text := entry.text(True)) not in entries
+    ]
+    if len(entries) == 0:
+        return '何も見つからなかった。'
+    return ''.join(entry + '\n\n' for entry in entries)
