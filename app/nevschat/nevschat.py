37acb95f0c8b71cbf20d269c5ed6950568015cf2
Nev Delap <nevdelap@gmail.com>
2024-05-14 19:45:31 +1000

    Bump version.

--- app/nevschat/nevschat.py
+++ app/nevschat/nevschat.py
@@ -6,7 +6,7 @@ from nevschat.state import State
 from reflex.style import color_mode  # type: ignore
 from reflex.style import toggle_color_mode
 
-VERSION = '0.0.112'
+VERSION = '0.0.113'
 TITLE = f'ネヴの素晴らしいチャットジーピーティー v{VERSION}'
 
 
