eca9daf612644e3f57ed21f2836f9dfb74f84df5
Nev Delap <nevdelap@gmail.com>
2024-05-17 17:35:40 +1000

    Fix long lines.

--- app/nevschat/canned_chat.py
+++ app/nevschat/canned_chat.py
@@ -12,7 +12,10 @@ def get_canned_chat() -> list[PromptResponse]:
                 pitch=0,
                 speaking_rate=1,
                 text='そうですか？',
-                tts_wav_url='wav/tts_ja-JP-Neural2-B_1.0_0.0_3a26eb6e7ff37e88b995d6bbe579cfa1.wav',
+                tts_wav_url=(
+                    'wav/tts_ja-JP-Neural2-B_1.0_0.0_'
+                    '3a26eb6e7ff37e88b995d6bbe579cfa1.wav'
+                ),
                 voice=get_default_voice(),
             ),
             response=Response(
@@ -21,7 +24,10 @@ def get_canned_chat() -> list[PromptResponse]:
                 pitch=0,
                 speaking_rate=1,
                 text='はい、そうです。',
-                tts_wav_url='wav/tts_ja-JP-Neural2-B_1.0_0.0_f01aa1dd97e1bc6798f739b4ab06094a.wav',
+                tts_wav_url=(
+                    'wav/tts_ja-JP-Neural2-B_1.0_0.0_'
+                    'f01aa1dd97e1bc6798f739b4ab06094a.wav'
+                ),
                 voice=get_default_voice(),
             ),
             editing=False,
@@ -32,7 +38,10 @@ def get_canned_chat() -> list[PromptResponse]:
                 pitch=0,
                 speaking_rate=1,
                 text='いいです？',
-                tts_wav_url='wav/tts_ja-JP-Neural2-B_1.0_0.0_4ff62a3d772146da7c906eaa2099fbe7.wav',
+                tts_wav_url=(
+                    'wav/tts_ja-JP-Neural2-B_1.0_0.0_'
+                    '4ff62a3d772146da7c906eaa2099fbe7.wav'
+                ),
                 voice=get_default_voice(),
             ),
             response=Response(
@@ -41,7 +50,10 @@ def get_canned_chat() -> list[PromptResponse]:
                 pitch=0,
                 speaking_rate=1,
                 text='はい、いいですよ！',
-                tts_wav_url='wav/tts_ja-JP-Neural2-B_1.0_0.0_ccba5ccc5754fca6737a099281019854.wav',
+                tts_wav_url=(
+                    'wav/tts_ja-JP-Neural2-B_1.0_0.0_'
+                    'ccba5ccc5754fca6737a099281019854.wav'
+                ),
                 voice=get_default_voice(),
             ),
             editing=False,
